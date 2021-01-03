85b0549fe0dfa33a50a895935dbc85b6c8f50aec
James <james.waugh.r@gmail.com>
2021-01-03 16:21:26 -0500

    Rewrite of initial source to support serving ShinMeiKai satisfactorily

--- Server.ts
+++ Server.ts
@@ -1,11 +1,41 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { expandGlobSync } from "https://deno.land/std@0.83.0/fs/mod.ts";
 
-const books = new Map<string, any>();
-books.set("1", {
-  id: "1",
-  title: "The Hound of the Baskervilles",
-  author: "Conan Doyle, Arthur",
-});
+console.log("a", Deno.cwd());
+
+interface YomichanDictEntry {
+  expression: string;
+  reading: string;
+  definitionTags: string;
+  rules: string;
+  score: string;
+  glossary: string[][];
+  sequence: string;
+  termTags: string;
+}
+
+const books = new Map<string, YomichanDictEntry>();
+
+for (const file of expandGlobSync("新明解国語辞典第五版v3/term_bank_*.json")) {
+  const content = Deno.readTextFileSync(file.path);
+  const json = JSON.parse(content) as any[][];
+  const dictEntries = json.map(
+    (entry) =>
+      ({
+        expression: entry[0],
+        reading: entry[1],
+        definitionTags: entry[2],
+        rules: entry[3],
+        score: entry[4],
+        glossary: (entry[5] as string[]).map((s) => s.split("\n")),
+        sequence: entry[6],
+        termTags: entry[7],
+      } as YomichanDictEntry)
+  );
+  dictEntries.forEach((e) => books.set(e.expression, e));
+}
+
+console.log("Ready to Run");
 
 const router = new Router();
 router
@@ -15,9 +45,15 @@ router
   .get("/book", (context) => {
     context.response.body = Array.from(books.values());
   })
+  .get("/bookcount", (context) => {
+    context.response.body = books.size;
+  })
   .get("/book/:id", (context) => {
-    if (context.params && context.params.id && books.has(context.params.id)) {
-      context.response.body = books.get(context.params.id);
+    const id = context.params?.id;
+    console.log(id);
+    if (id && books.has(id)) {
+      console.log("OK");
+      context.response.body = books.get(id);
     }
   });
 
